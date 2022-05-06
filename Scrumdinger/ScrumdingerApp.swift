48e25ee386d7fed41a5e7addf858990e3c2d792a
VictorTamayoBriones <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-05-06 09:51:20 -0500

    design view

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -1,17 +1,10 @@
-//
-//  ScrumdingerApp.swift
-//  Scrumdinger
-//
-//  Created by Victor Tamayo on 05/05/22.
-//
-
 import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MeetingView()
         }
     }
 }
