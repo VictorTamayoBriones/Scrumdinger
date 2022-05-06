57499f7c4d1a2cb7b0631b770b55c73818f26e89
VictorTamayoBriones <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-05-05 19:16:57 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Victor Tamayo on 05/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
