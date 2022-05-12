13a511c8af88fdb3886bca3743e7ed85412ef849
VictorTamayoBriones <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-05-11 21:07:37 -0500

    navigation

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -6,15 +6,37 @@ struct DetailView: View {
     var body: some View {
         List{
             Section(header: Text("Meeting Info")){
-                Label("Start Meeting", systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
-                    .accessibilityLabel("Click Here Start Meeting")
+                NavigationLink(destination: MeetingView()){
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                        .accessibilityLabel("Click Here Start Meeting")
+                }
+
                 HStack{
                     Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack{
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text("\(scrum.theme.name)")
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+            Section(header: Text("Attendees")){
+                ForEach(scrum.attendees){attendee in
+                    Label(attendee.name, systemImage: "person")
                 }
             }
         }
+        .navigationTitle(scrum.title)
     }
 }
 
